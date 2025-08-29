0ae497bed6ec5d67fccdaafa05d51c9e3e4fdd3b
Carlos Canete <cdcaneteu@ine.gob.cl>
2025-08-29 01:25:24 -0400

    subor gitignore

--- obj/Debug/net8.0/RandomNumberApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/RandomNumberApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RandomNumberApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbef952e3120f892f816372149a1dde73eaf7ccc")]
 [assembly: System.Reflection.AssemblyProductAttribute("RandomNumberApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RandomNumberApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
